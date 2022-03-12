a7217bd47cdc80d96cb493bbfc1e34ca0f0c04d4
Vladlena Skubish <vskubish1410@bk.ru>
2022-03-12 15:48:58 +0300

    ex00 done

--- cpp_module00/ex00/megaphone.cpp
+++ cpp_module00/ex00/megaphone.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <ctype.h>
-#include <algorithm>
-
-// using namespace std; // so that compiler uses standard library std, its namespaces BUT is forbidden in project
+#include <cstring> 
 
 char ft_toupper_cpp(const char sy) {
     if (sy <= 'z' && sy >= 'a')
@@ -10,18 +8,43 @@ char ft_toupper_cpp(const char sy) {
     return sy;
 }
 
+void empty_message() {
+	std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+}
+
 int main(int argc, const char * argv[]) {
+	int empty = 0;
+	
 	if (argc < 2) {
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		empty_message();
 		return 0;
 	}
 	for (int i = 1; i < argc; i++) {
-		std::string data(argv[i]);
-		std::transform(data.begin(), data.end(), data.begin(), ft_toupper_cpp);
-		// std::transform(data.begin(), data.end(), data.begin(), [](unsigned char c) -> unsigned char { return std::toupper(c); });
-		// std::transform(data.begin(), data.end(), data.begin(), toupper);
-		std::cout << data;
+		if (!argv[i] || !argv[i][0]) {
+			empty++;
+		}
+		else {
+			std::string data(argv[i]);
+			size_t j = 0;
+			size_t len = strlen(argv[i]);
+
+			while (j < len) {
+				data[j] = ft_toupper_cpp(data[j]);
+				j++;
+			}
+			std::cout << data;
+			//нужен ли деструктор data и как его записать?
+		}
 	}
-	std::cout << std::endl;
+	if (empty == argc - 1) {
+		empty_message();
+	}
+	else {
+		std::cout << std::endl;
+	}
 	return 0;
 }
+
+// https://www.cplusplus.com/reference/string/string/
+// #include <typeinfo>
+// std::cout << typeid(j).name() << std::endl;
