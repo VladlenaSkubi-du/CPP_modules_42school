f9ae3bb184ff6aeef20f677bd17345e655ee1ecc
Vladlena Skubish <vskubish1410@bk.ru>
2022-03-26 20:01:34 +0300

    method add is needed

--- cpp_module00/ex01/phoneBook.hpp
+++ cpp_module00/ex01/phoneBook.hpp
@@ -4,16 +4,38 @@
 #pragma once /* Защита от двойного подключения заголовочного файла */
 #include <iostream>
 #include <map>
+#include <iomanip>
+#include <ctime>
 
-class PhoneBook {
+class PhoneBookContact {
+	private:
+		short	ContactID;
+		std::string FirstName;
+		std::string LastName;
+		std::string Nickname;
+		std::string	PhoneNumber; 
+		std::string	DarkestSecret;
 	public:
-		void add_to_phonebook(const std::string&, const std::string&);
-		std::string look_in_phonebook(const std::string&);
-	// private:
-	// 	std::map<int, std::string> names;
-	// 	std::map<int, std::string> phones;
-	// найти по id номер телефона
+		PhoneBookContact(void);
+		PhoneBookContact(short id, const std::string& fn, const std::string& sn, const std::string& nick,
+			const std::string& phone, const std::string& secret);
+		~PhoneBookContact(void);
+		void		display_while_search(void);
+		// int			add_contact(const std::string&, const std::string&);
+		// void		display_contact_information(void);
 };
 
+class PhoneBook {
+	private:
+		PhoneBookContact	YourContact[8]; //оператор равно - обработка
+	public:
+		PhoneBook(void); //можно делать сколько угодно конструкторов
+		~PhoneBook(void);
+		short	print_phonebook_contacts(void);
+		// void	add_to_phonebook(void);
+		// void	display_contact_by_id(short);
+};
+
+//геттеры и сеттеры
 
 #endif
